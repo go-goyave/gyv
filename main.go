dfc61742b8ab5e2a4320caad883e1092da3c7fdb
LAURENT Louis <louis_laurent@aol.fr>
2021-07-04 16:27:25 +0200

    fix root command name

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func buildRootCommand() *cobra.Command {
 	gyv := &cobra.Command{
-		Use:     "cli-sample",
+		Use:     "gyv",
 		Version: "0.1.0",
 		Short:   "Productivity CLI for the Goyave framework",
 		Long: `gyv productivity command-line interface for the Goyave framework.
